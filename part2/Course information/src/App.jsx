ccab74dc2cfc415cbc7f685811dc9bcbda045951
SirGram <119138378+SirGram@users.noreply.github.com>
2023-11-28 12:55:50 +0100

    starting part2

--- part2/Course information/src/App.jsx
+++ part2/Course information/src/App.jsx
@@ -1,45 +1,6 @@
-const Part = (props)=> {
-  return(
-    <p>{props.part.name} {props.part.exercises}</p>
-  )
-}
-const Total =(props)=> {
- 
-  const exerciseValues = props.parts.map((part)=>part.exercises)
-  let initialValue=0
+import Course from "./components/Course";
 
-  console.log(exerciseValues)
-  const total = exerciseValues.reduce((accumulator, currentValue) =>  accumulator+currentValue, initialValue)
-  console.log(total);
-  
-  return(
-    <h4>Total of {total} exercises</h4>
-  )
-}
-
-const Course = (props) => {
-  return (
-    <>
-      <h1>"Web development curriculum"</h1>
-      {props.courses.map((course)=>(
-        <div key = {course.id}>
-        <h2>{course.name}</h2>
-
-        <div>        
-          {course.parts.map((part) =>
-            <Part key= {part.id} part = {part}/>
-          )}
-          <Total parts={course.parts}/>
-        </div>  
-      </div> 
-
-      ))}
-    </>
-    
-  )
-}
-
-const App = () => {
+const App = (Course) => {
   const courses = [
     {
       name: 'Half Stack application development',
